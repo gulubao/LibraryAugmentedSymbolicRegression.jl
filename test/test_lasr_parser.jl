776196e339191736e20d4eb6eed7c468aa6e4472
Atharva Sehgal <atharva.sehgal@gmail.com>
2025-08-29 07:01:03 -0700

    Update SR.jl dependencies (#67)
    * update SR.jl dependency
    
    * update version
    
    * replacing custom parser with DynamicExpressions parser
    
    * update more dependencies
    
    * update project.toml
    
    * update julia version

--- test/test_lasr_parser.jl
+++ test/test_lasr_parser.jl
@@ -4,12 +4,15 @@ println("Testing LaSR expression parser")
 
 using Random: MersenneTwister
 using LibraryAugmentedSymbolicRegression:
-    LaSROptions, string_tree, parse_expr, render_expr, gen_random_tree
+    LaSROptions, string_tree, parse_expr, gen_random_tree
 include("test_params.jl")
 
 @inline safepow(x, y) = sign(x) * abs(x)^y
 options = LaSROptions(;
-    default_params..., binary_operators=[-, +, *, safepow], unary_operators=[sin, cos, exp]
+    default_params...,
+    binary_operators=[-, +, *, safepow],
+    unary_operators=[sin, cos, exp],
+    variable_names=Dict('x' * string(i) => ('x' * string(i)) for i in 1:9),
 )
 
 rng = MersenneTwister(314159)
@@ -28,7 +31,8 @@ for depth in [5, 9]
             @test str_tree == String(strip(str_tree, [' ', '\n', '"', ',', '.', '[', ']']))
             expr_tree = parse_expr(T, str_tree, options)
             expr_output = expr_tree(data, options.operators)
-            @test isapprox(expr_output, output)
+            @test string_tree(expr_tree) == str_tree # "[$i] String representation mismatch: $(string_tree(expr_tree)) vs $str_tree for tree: $str_tree"
+            @test isapprox(expr_output, output) # "[$i] Output mismatch: $(expr_output) vs $(output) for tree: $str_tree"
         end
     end
 end
